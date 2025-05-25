ee4f783287f7f2474c0861c0e452f2f45a541319
Jakob Calvén <jakob.calven@gmail.com>
2025-05-25 13:30:18 -0700

    Add LatLng model to represent geographical coordinates.
    - 🌍 Introduced a new LatLng class for latitude and longitude representation.
    - 📐 Class includes required parameters for initialization.
    - 🔄 Overrides toString method for easy string representation of coordinates.

---
+++ lib/src/models/utils/lat_lng.dart
@@ -0,0 +1,9 @@
+class LatLng {
+  final double latitude;
+  final double longitude;
+
+  LatLng({required this.latitude, required this.longitude});
+
+  @override
+  String toString() => '$latitude,$longitude';
+}
