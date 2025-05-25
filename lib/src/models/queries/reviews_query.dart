2b95b7c27d7511bb14453430409604cc90c7edcc
Jakob Calvén <jakob.calven@gmail.com>
2025-05-24 20:39:06 -0700

    Deprecate the old ReviewsQuery factory constructor in favor of distinct query types. This change enhances code clarity by encouraging the use of specific query classes (withCid, withFid, withPlaceId) instead of a generic constructor.
    - 🗑️ Marked the old ReviewsQuery constructor as deprecated
    - 🔍 Improved clarity by promoting specific query types
    - 📜 Updated documentation for better understanding

--- lib/src/models/queries/reviews_query.dart
+++ lib/src/models/queries/reviews_query.dart
@@ -78,36 +78,37 @@ sealed class ReviewsQuery with _$ReviewsQuery {
     String? q,
   }) = ReviewsQueryPlaceId;
 
-  // const factory ReviewsQuery({
-  //   /// {@macro QueryDocTemplates.cidDoc}
-  //   required String cid,
+  @deprecated
+  const factory ReviewsQuery({
+    /// {@macro QueryDocTemplates.cidDoc}
+    required String cid,
 
-  //   /// {@macro QueryDocTemplates.fidDoc}
-  //   required String fid,
+    /// {@macro QueryDocTemplates.fidDoc}
+    required String fid,
 
-  //   /// {@macro QueryDocTemplates.placeIdDoc}
-  //   required String placeId,
+    /// {@macro QueryDocTemplates.placeIdDoc}
+    required String placeId,
 
-  //   /// {@macro QueryDocTemplates.glDoc}
-  //   String? gl,
+    /// {@macro QueryDocTemplates.glDoc}
+    String? gl,
 
-  //   /// {@macro QueryDocTemplates.hlDoc}
-  //   String? hl,
+    /// {@macro QueryDocTemplates.hlDoc}
+    String? hl,
 
-  //   /// Optional parameter to sort reviews
-  //   ///
-  //   /// Possible values: 'newest', 'highest_rating', 'lowest_rating', 'relevant'
-  //   String? sortBy,
+    /// Optional parameter to sort reviews
+    ///
+    /// Possible values: 'newest', 'highest_rating', 'lowest_rating', 'relevant'
+    String? sortBy,
 
-  //   /// Optional topic ID to filter reviews by topic
-  //   String? topicId,
+    /// Optional topic ID to filter reviews by topic
+    String? topicId,
 
-  //   /// Optional token for pagination
-  //   String? nextPageToken,
+    /// Optional token for pagination
+    String? nextPageToken,
 
-  //   /// Optional query string to filter reviews
-  //   String? q,
-  // }) = _ReviewsQuery;
+    /// Optional query string to filter reviews
+    String? q,
+  }) = _ReviewsQuery;
 
   factory ReviewsQuery.fromJson(Map<String, dynamic> json) =>
       _$ReviewsQueryFromJson(json);
